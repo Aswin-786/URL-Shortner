1b0bebb5641a91bed787b4161a5bdd88215f9761
Aswin <aswinkrishna363@gmail.com>
2023-12-11 13:42:17 +0530

    attached authmodule for protecting route

--- backend/src/url/url.module.ts
+++ backend/src/url/url.module.ts
@@ -3,9 +3,13 @@ import { UrlController } from './url.controller';
 import { UrlService } from './url.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { URLSchema } from './schemas/url.schemas';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'URL', schema: URLSchema }])],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([{ name: 'URL', schema: URLSchema }]),
+  ],
   controllers: [UrlController],
   providers: [UrlService],
 })
