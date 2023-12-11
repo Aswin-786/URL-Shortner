4f9cc9ab6b1e9cef22389240132cbced2b94ff5a
Aswin <aswinkrishna363@gmail.com>
2023-12-11 13:44:26 +0530

    modified

--- backend/src/url/url.service.ts
+++ backend/src/url/url.service.ts
@@ -3,21 +3,27 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { URL } from './schemas/url.schemas';
 import { nanoid } from 'nanoid';
-import { log } from 'console';
 
 @Injectable()
 export class UrlService {
   constructor(@InjectModel('URL') private readonly urlModel: Model<URL>) {}
 
+  //creating short url
   async createShortURL(originalURL: string): Promise<string> {
+    //checking the url
+    const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/i;
+    if (!urlPattern.test(originalURL)) {
+      throw new Error('Enter a valid URL');
+    }
+
     const existingURL = await this.urlModel.findOne({ originalURL }).exec();
     if (existingURL) {
       return existingURL.shortURL;
     } else {
+      //using nanoid
       const urlId = nanoid(8);
       const base = `http://localhost:9000`;
       const shortURL = `${base}/${urlId}`;
-      console.log(shortURL);
 
       const newURL = new this.urlModel({ originalURL, shortURL });
       await newURL.save();
@@ -25,11 +31,10 @@ export class UrlService {
     }
   }
 
+  //redirect the url
   async redirectToOriginalURL(shortURL: string): Promise<string> {
-    console.log(shortURL);
     const base = `http://localhost:9000`;
     const url = `${base}/${shortURL}`;
-
     const existingURL = await this.urlModel.findOne({ shortURL: url }).exec();
     if (!existingURL) {
       throw new NotFoundException('Short URL not found');
